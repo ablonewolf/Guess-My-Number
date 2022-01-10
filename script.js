76a8f609b3d7f69b031616d9ecf9f7116451137b
ab <arkabhuiyan66@gmail.com>
2022-01-10 22:24:58 +0600

    changing bg and adding 'again' button functionality

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 "use strict";
 
-const secret_number = Math.trunc(Math.random() * 20) + 1;
+let secret_number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 //console.log(secret_number);
-document.querySelector(".number").textContent = secret_number;
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".score").textContent = score;
+  secret_number = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".number").textContent = "?";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector("number").style.width = "15rem";
+});
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -13,15 +24,30 @@ document.querySelector(".check").addEventListener("click", function () {
     score++;
     document.querySelector(".message").textContent = "✌️✌️ Correct Number!!";
     document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent = secret_number;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
   } else {
-    if (score > 1) {
-      score--;
-      document.querySelector(".message").textContent = "😢😢 You are wrong!!";
-      document.querySelector(".score").textContent = score;
+    if (guess < secret_number) {
+      if (score > 1) {
+        score--;
+        document.querySelector(".message").textContent = "😢😢 Too Low!!";
+        document.querySelector(".score").textContent = score;
+      } else {
+        document.querySelector(".message").textContent =
+          "😭😭 You lost the game!!";
+        document.querySelector(".score").textContent = 0;
+      }
     } else {
-      document.querySelector(".message").textContent =
-        "😭😭 You lost the game!!";
-      document.querySelector(".score").textContent = 0;
+      if (score > 1) {
+        score--;
+        document.querySelector(".message").textContent = "😢😢 Too High!!";
+        document.querySelector(".score").textContent = score;
+      } else {
+        document.querySelector(".message").textContent =
+          "😭😭 You lost the game!!";
+        document.querySelector(".score").textContent = 0;
+      }
     }
   }
 });
