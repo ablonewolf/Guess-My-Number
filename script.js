ac2cb7c042b877eca298d81aa633c6b8d9f01d2f
ab <arkabhuiyan66@gmail.com>
2022-01-10 13:23:43 +0600

    Added event listeners part-1

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-'use strict';
+"use strict";
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number";
+  }
+});
