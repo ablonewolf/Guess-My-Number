8dddf0d48204422863dc25473f77d5673ca9e1e6
ab <arkabhuiyan66@gmail.com>
2022-01-10 20:45:51 +0600

    Displaying scores and comparing the input

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 "use strict";
 
+const secret_number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+//console.log(secret_number);
+document.querySelector(".number").textContent = secret_number;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   if (!guess) {
-    document.querySelector(".message").textContent = "No number";
+    document.querySelector(".message").textContent = "😕😕 No number";
+  } else if (guess === secret_number) {
+    score++;
+    document.querySelector(".message").textContent = "✌️✌️ Correct Number!!";
+    document.querySelector(".score").textContent = score;
+  } else {
+    if (score > 1) {
+      score--;
+      document.querySelector(".message").textContent = "😢😢 You are wrong!!";
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "😭😭 You lost the game!!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
